b4d83a7ff2ca7e2e33d04dece2562dcde672038e
mariaobis <108530298+mariaobis@users.noreply.github.com>
2024-05-13 10:54:04 -0300

    s

--- main.js
+++ main.js
@@ -189,7 +189,6 @@ const tempoObjetivo1 = new Date("2024-12-10T00:00:00");
 const tempoObjetivo2 = new Date("2024-11-17T00:00:00");
 const tempoObjetivo3 = new Date("2028-12-06T00:00:00");
 const tempoObjetivo4 = new Date("2034-01-15T00:00:00");
-const tempoObjetivo5 = new Date("2025-06-27T00:00:00");
 
 //automatizar o código, criar uma variável chamada tempos, que receberá uma lista.
 const tempos =
